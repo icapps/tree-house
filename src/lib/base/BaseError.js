5541a0da0b36ab2251de70ec5f9d30fa29547a46
Anthony Madhvani <anthony.madhvani@icapps.be>
2017-08-17 20:24:24 +0200

    Remove typeof check for Error.captureStackTrace

--- src/lib/base/BaseError.js
+++ src/lib/base/BaseError.js
@@ -2,10 +2,6 @@ export default class BaseError extends Error {
     constructor(message) {
         super(message);
         this.name = this.constructor.name;
-        if (typeof Error.captureStackTrace === 'function') {
-            Error.captureStackTrace(this, this.constructor);
-        } else {
-            this.stack = (new Error(message)).stack;
-        }
+        Error.captureStackTrace(this, this.constructor);
     }
 }
