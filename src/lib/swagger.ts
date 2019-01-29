d6f478e133d449b0329b8937599f6827a9a39a4c
ben-vr <ben.vanraemdonck@icapps.com>
2019-01-29 10:04:21 +0100

    commit for symbolic link

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -8,15 +8,40 @@ import * as fs from 'fs';
  */
 export function setSwagger(app: Application, route: string, filePath: string, options: SwaggerOptions = {}): void {
   try {
-    // Load yaml document
-    const swaggerDocument = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
-    Object.assign(swaggerDocument, options);
+    const stats = fs.lstatSync(filePath);
+    let swaggerDocument;
+
+    if (options.concatenate) {
+      // Throw error if concatenate = true and filepath = file
+      if (stats.isFile()) {
+        throw new Error('Boolean concatenate cannot be true when you specify a file. When you want to concatenate, specify a folder');
+      }
+      if (stats.isDirectory()) {
+        swaggerDocument = buildSwaggerDocumentFromFiles(filePath);
+      }
+    } else {
+      if (stats.isFile()) {
+        // Load yaml document
+        swaggerDocument = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
+        Object.assign(swaggerDocument, options);
+      }
+      if (stats.isDirectory()) {
+        throw new Error('To concatenate a folder of swagger YMLS, you need to explicitly set the boolean concatenate on true for the swaggerOptions');
+      }
+    }
+
+    const useSchema = schema => (...args) => swaggerUi.setup(schema)(...args);
+    console.log('Testing symbolic link');
 
     // Serve the document served via swagger-ui
-    app.use(route, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use(route, swaggerUi.serve, useSchema(swaggerDocument));
   } catch (e) {
     throw new Error(`Failed to load swagger documentation: ${e}`);
   }
+}
+
+function buildSwaggerDocumentFromFiles(_filePath: string) {
+
 }
 
 // Interfaces
@@ -31,4 +56,5 @@ export interface SwaggerOptions {
   swaggerOptions?: {
     validatorUrl?: string | null,
   };
+  concatenate?: boolean;
 }
